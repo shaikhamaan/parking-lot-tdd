b34adf19312797f9d5a65cea765bf0b9616006a8
shaikhamaan <amaans@sahaj.ai>
2023-02-17 00:50:04 +0530

    update constants and factory

--- src/main/kotlin/Factory.kt
+++ src/main/kotlin/Factory.kt
@@ -1,22 +1,35 @@
+import Constants.FARE
+import models.Receipt
 import models.Ticket
 import models.Vehicle
+import utils.Utils
+import java.util.*
 
 object Factory {
     private var vehicleCount = 0
     private var ticketCount = 0
 
-    fun getVehicle(vehicleType: String): Vehicle {
+    fun generateVehicle(vehicleType: String): Vehicle {
         return Vehicle(vehicleType, ++vehicleCount)
     }
 
-    fun getTicket(vehicleId: Int): Ticket {
+    fun generateTicket(vehicleId: Int): Ticket {
         return Ticket(vehicleId, ++ticketCount)
+    }
+
+    fun generateReceipt(ticket: Ticket): Receipt {
+        val receipt = Receipt(ticket.vehicleId, ticket.ticketId, ticket.entryTime)
+        val fare = generateFare(ticket)
+        receipt.setFare(fare)
+        return receipt
+    }
+
+    private fun generateFare(ticket: Ticket): Int {
+        return Utils.getHourDifference(ticket.entryTime, Date()) * FARE
     }
 
     fun reset() {
         vehicleCount = 0
         ticketCount = 0
     }
-
-
 }
