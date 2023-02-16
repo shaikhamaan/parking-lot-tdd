3f87637b39cc79dec39953cc363740a2fb984dc7
shaikhamaan <amaans@sahaj.ai>
2023-02-17 00:47:54 +0530

    update vehicle and its tests

--- src/main/kotlin/models/Vehicle.kt
+++ src/main/kotlin/models/Vehicle.kt
@@ -1,6 +1,9 @@
 package models
 
 import Factory
+import exceptions.NoSpotAvailableException
+import exceptions.VehicleNotParkedException
+import repositories.ParkingLot
 
 data class Vehicle(
     var type: String,
@@ -11,11 +14,24 @@ data class Vehicle(
             throw NoSpotAvailableException("No spot available to park")
         }
 
-        val ticket = Factory.getTicket(id)
+        val ticket = Factory.generateTicket(id)
 
         ParkingLot.reduceAvailableSpotsByOne()
         ParkingLot.addToParkingList(this)
 
         return ticket
+    }
+
+    fun unPark(ticket: Ticket): Receipt {
+        if(!ParkingLot.isVehicleParked(id)) {
+            throw VehicleNotParkedException("Vehicle with given ID is not parked")
+        }
+
+        val receipt = Factory.generateReceipt(ticket)
+
+        ParkingLot.increaseAvailableSpotsByOne()
+        ParkingLot.removeFromParkingList(this)
+
+        return receipt
     }
 }
--- src/test/kotlin/models/VehicleTest.kt
+++ src/test/kotlin/models/VehicleTest.kt
@@ -1,24 +1,56 @@
 package models
 
 import Factory
+import exceptions.NoSpotAvailableException
+import exceptions.VehicleNotParkedException
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import repositories.ParkingLot
 
 class VehicleTest {
     @AfterEach
     fun tearDown() {
+        ParkingLot.reset()
         Factory.reset()
     }
 
     @Test
     fun `it should park a vehicle`() {
-        val car = Factory.getVehicle("Car")
+        val car = Factory.generateVehicle("Car")
         val ticket: Ticket = car.park()
         val parkingList = ParkingLot.getParkingList()
 
         assertEquals(1, ticket.vehicleId)
         assertEquals(1, ticket.ticketId)
         assertEquals(parkingList[ticket.vehicleId], car)
+    }
+
+    @Test
+    fun `it should unpark a vehicle`() {
+        val car = Factory.generateVehicle("Car")
+        val ticket: Ticket = car.park()
+        val receipt: Receipt = car.unPark(ticket)
+
+        assertEquals(0, receipt.getFare())
+    }
+
+    @Test
+    fun `it should throw no vehicle parked exception`() {
+        val car = Factory.generateVehicle("Car")
+        val ticket = Factory.generateTicket(car.id)
+
+        assertThrows<VehicleNotParkedException> { car.unPark(ticket) }
+    }
+
+    @Test
+    fun `it should throw no spot available exception`() {
+        val car = Factory.generateVehicle("Car")
+
+        for (i in 0..99) {
+            ParkingLot.reduceAvailableSpotsByOne()
+        }
+        assertThrows<NoSpotAvailableException> { car.park() }
     }
 }
